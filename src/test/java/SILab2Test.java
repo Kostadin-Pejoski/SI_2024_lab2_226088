7ce73bfb152446b2061684fec35038adb38a2311
Kostadin-Pejoski <kostadin.pejoski1@gmail.com>
2024-05-12 12:41:57 +0200

    added all files

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+    private final SILab2 lab = new SILab2();
+
+    @Test
+    public void allTests(){
+
+    }
+}
